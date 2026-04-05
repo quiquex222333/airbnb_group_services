17f509ef6bdc2c3c154e59f97e23e4d79abae616
Oscar Alvaro Terceros Beltrán <osterce@outlook.com>
2026-04-05 02:25:12 -0400

    chore: update default Cognito client ID and user pool ID with production values

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export const env = {
   PORT: process.env.PORT || 3000,
   AWS_REGION: process.env.AWS_REGION || 'us-east-1',
-  COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID || 'dummy_client_id',
-  COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID || 'dummy_user_pool_id',
+  COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID || 'krh0pe28ldco9bt0q2s7vb1cl',
+  COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID || 'us-east-1_bkvBFyJeF',
   JWT_SECRET_MOCK: process.env.JWT_SECRET_MOCK || 'super_secret', // Usado solo si se hace mock de JWKS local
 };
